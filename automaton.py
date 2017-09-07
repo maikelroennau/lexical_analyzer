f83dc33ee066da912149c18fcb369eccaf273ba7
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-09-07 20:57:50 -0300

    Updated automaton. Added new methods to Automaton class.

--- automaton.py
+++ automaton.py
@@ -21,7 +21,28 @@ class Automaton:
                 return False
 
         return state in self.final_states
-        
+
+    def validate_identifier(self, word):
+        state = self.initial_state
+
+        for letter in word:
+            try:
+                state = self.graph[state][letter]
+            except KeyError:
+                return False
+
+        if state in self.final_states:
+            return state
+        else:
+            return False
+            
+    def get_transitions(self, state):
+        if state in self.graph.keys():
+            return self.graph[state]
+
+    def get_transitions_values(self, state):
+        if state in self.graph.keys():
+            return self.graph[state].keys()
 
     def set_initial_state(self, state):
         self.initial_state = state
--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -3,7 +3,31 @@ from string import ascii_lowercase, ascii_uppercase
 from automaton import Automaton
 
 
-def get_validation(automaton, words):
+def get_validation(automaton, words, primitives, identifiers):
+    print '\nValidating words: ' + ', '.join(words) + '\n'
+
+    symbols_table = {}
+    error_list = []
+
+    for i, word in enumerate(words):
+        result = automaton.validate_identifier(word)
+        
+        if result is not False:
+            print '[{}] {}'.format(i+1, primitives[result])
+            
+            if primitives[result] in identifiers:
+                if word not in symbols_table.keys():
+                    symbols_table[word] = len(symbols_table.keys())
+        else:
+            error_list.append(str(i+1))
+
+    print '\nTable of symbols:'
+    for i, symbol in enumerate(symbols_table.keys()):
+        print '{} - {}'.format(i+1, symbol)
+
+    print '\nThe program has erros in the lines: {}'.format(', '.join(error_list))
+
+def validate_words(automaton, words):
     print '\nValidating words: ' + ', '.join(words) + '\n'
 
     for i, word in enumerate(words):
@@ -11,7 +35,6 @@ def get_validation(automaton, words):
             print 'Accept[{}]: {}'.format(i + 1, word)
         else:
             print 'Reject[{}]: {}'.format(i + 1, word)
-
 
 def read_text_file(path):
     with open(path, 'r') as file:
@@ -28,6 +51,11 @@ def get_alphabet(state):
 
 def run():
     automaton = Automaton('q1', {'q3', 'q5', 'q7', 'q8', 'q10', 'q14', 'q17', 'q21', 'q24', 'q27', 'q32', 'q35', 'q40'})
+
+    primitives = {'q3':'integer number', 'q5':'real number', 'q7':'comment', 'q8':'identifier', 'q10':'int', 'q14':'float',
+                   'q17':'real', 'q21':'break', 'q24':'char', 'q27':'case', 'q32':'double', 'q35':'const', 'q40':'continue'}
+    
+    identifiers = ['identifier', 'integer number', 'real number']
 
     automaton.add_state('q1', {'+': 'q2', '-': 'q3'})
     automaton.add_state('q1', get_numerals('q3'))
@@ -51,9 +79,9 @@ def run():
     automaton.add_state('q7', get_alphabet('q7'))
 
     automaton.add_state('q8', {'n': 'q9', 'l': 'q11', 'e': 'q15', 'r': 'q18', 'h': 'q22', 'a': 'q25', 'o': 'q28'})
+    automaton.add_state('q8', { i:'q8' for i in list(set(get_alphabet('q8')) - set(automaton.get_transitions_values('q8'))) })
     automaton.add_state('q8', get_numerals('q8'))
-    automaton.add_state('q8', get_alphabet('q8'))
-
+    
     automaton.add_state('q9', {'t': 'q10'})
 
     automaton.add_state('q10', get_numerals('q8'))
@@ -98,7 +126,7 @@ def run():
     automaton.add_state('q27', get_numerals('q8'))
     automaton.add_state('q27', get_alphabet('q8'))
 
-    automaton.add_state('q28', {'u': 'q29', 'n': '33'})
+    automaton.add_state('q28', {'u': 'q29', 'n': 'q33'})
 
     automaton.add_state('q29', {'b': 'q30'})
 
@@ -127,14 +155,8 @@ def run():
     automaton.add_state('q40', get_numerals('q8'))
     automaton.add_state('q40', get_alphabet('q8'))
 
-    print '\nAutomaton'
-    automaton.show_graph()
-
     words = read_text_file('words.txt')
-    get_validation(automaton, words)
-
-    # Errors: 99., real a, double _c, as_asd, -- Teste, Tales Viegas --
-
+    get_validation(automaton, words, primitives, identifiers)
 
 if __name__ == '__main__':
     run()
