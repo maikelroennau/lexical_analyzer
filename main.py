b8c1a123e48286527851c0c8721811d4bc302f15
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-09-16 23:31:50 -0300

    Corrected class name.

--- main.py
+++ main.py
@@ -1,90 +1,90 @@
 import string
-from automaton import Automaton
+from automata import Automata
 
 
-def get_validation(automaton, words):
+def get_validation(automata, words):
         print '\nValidating words: ' + ', '.join(words) + '\n'
 
         for word in words:
-            if automaton.validate(word):
+            if automata.validate(word):
                 print 'Accept: {}'.format(word)
             else:
                 print 'Reject: {}'.format(word)
 
 def run():
-    print '\nAutomaton 1: real numbers'
-    automaton = Automaton('q1', {'q4'})
+    print '\nAutomata 1: real numbers'
+    automata = Automata('q1', {'q4'})
 
-    automaton.add_state('q1', {'+':'q2', '-':'q2'})
-    automaton.add_state('q1', {str(i):'q3' for i in range(0, 10)})
+    automata.add_state('q1', {'+':'q2', '-':'q2'})
+    automata.add_state('q1', {str(i):'q3' for i in range(0, 10)})
 
-    automaton.add_state('q2', {str(i):'q3' for i in range(0, 10)})
+    automata.add_state('q2', {str(i):'q3' for i in range(0, 10)})
 
-    automaton.add_state('q3', {str(i):'q3' for i in range(0, 10)})
-    automaton.add_state('q3', {'.':'q4'})
+    automata.add_state('q3', {str(i):'q3' for i in range(0, 10)})
+    automata.add_state('q3', {'.':'q4'})
 
-    automaton.add_state('q4', {str(i):'q4' for i in range(0, 10)})
+    automata.add_state('q4', {str(i):'q4' for i in range(0, 10)})
 
-    automaton.show_graph()
+    automata.show_graph()
 
     words = ['1.0', '4.2', '4.', '56.9', '0.53', '0.55593', '4582580.99553', '+12.0', '-1548.844', '+4559.66', '-9984.98554455', '1..2', '1.55.5']
-    get_validation(automaton, words)
+    get_validation(automata, words)
 
 
-    print '\nAutomaton 2: quoted text'
-    automaton.clear_all_states()
-    automaton.set_initial_state('q1')
-    automaton.set_final_states({'q3'})
+    print '\nAutomata 2: quoted text'
+    automata.clear_all_states()
+    automata.set_initial_state('q1')
+    automata.set_final_states({'q3'})
 
-    automaton.add_state('q1', {'\"':'q2'})
+    automata.add_state('q1', {'\"':'q2'})
 
-    automaton.add_state('q2', {i:'q2' for i in (string.ascii_lowercase + string.ascii_uppercase)})
-    automaton.add_state('q2', {' ':'q2', '\"':'q3'})
+    automata.add_state('q2', {i:'q2' for i in (string.ascii_lowercase + string.ascii_uppercase)})
+    automata.add_state('q2', {' ':'q2', '\"':'q3'})
     
-    automaton.add_state('q3', {}) # even it is empty, it should be added
+    automata.add_state('q3', {}) # even it is empty, it should be added
 
-    automaton.show_graph()
+    automata.show_graph()
 
     words = ['""', '"Maikel"', '"Maikel Maciel Ronnau"', 'Maikel', 'Maikel\"', '\"']
-    get_validation(automaton, words)
+    get_validation(automata, words)
 
 
-    print '\nAutomaton 3: commented text in C style'
-    automaton.clear_all_states()
-    automaton.set_initial_state('q1')
-    automaton.set_final_states({'q5'})
+    print '\nAutomata 3: commented text in C style'
+    automata.clear_all_states()
+    automata.set_initial_state('q1')
+    automata.set_final_states({'q5'})
 
-    automaton.add_state('q1', {'/':'q2'})
+    automata.add_state('q1', {'/':'q2'})
 
-    automaton.add_state('q2', {'*':'q3'})
+    automata.add_state('q2', {'*':'q3'})
 
-    automaton.add_state('q3', {i:'q3' for i in (string.ascii_lowercase + string.ascii_uppercase)})
-    automaton.add_state('q3', {' ':'q3', '*':'q4'})
+    automata.add_state('q3', {i:'q3' for i in (string.ascii_lowercase + string.ascii_uppercase)})
+    automata.add_state('q3', {' ':'q3', '*':'q4'})
 
-    automaton.add_state('q4', {'/':'q5'})
+    automata.add_state('q4', {'/':'q5'})
 
-    automaton.show_graph()
+    automata.show_graph()
 
     words = ['/**/', '/*Maikel*/', '/* Maikel Maciel Ronnau */', '/*', '**', '/* Maikel *', '/* Maikel /']
-    get_validation(automaton, words)
+    get_validation(automata, words)
 
 
-    print '\nAutomaton 4: integer numbers'
-    automaton.clear_all_states()
-    automaton.set_initial_state('q1')
-    automaton.set_final_states({'q3'})
+    print '\nAutomata 4: integer numbers'
+    automata.clear_all_states()
+    automata.set_initial_state('q1')
+    automata.set_final_states({'q3'})
 
-    automaton.add_state('q1', {'+':'q2', '-':'q2'})
-    automaton.add_state('q1', {str(i):'q3' for i in range(0, 10)})
+    automata.add_state('q1', {'+':'q2', '-':'q2'})
+    automata.add_state('q1', {str(i):'q3' for i in range(0, 10)})
 
-    automaton.add_state('q2', {str(i):'q3' for i in range(0, 10)})
+    automata.add_state('q2', {str(i):'q3' for i in range(0, 10)})
 
-    automaton.add_state('q3', {str(i):'q3' for i in range(0, 10)})
+    automata.add_state('q3', {str(i):'q3' for i in range(0, 10)})
 
-    automaton.show_graph()
+    automata.show_graph()
 
     words = ['0', '0001', '123', '1249745', '+12', '-1548', '1.2', '+4559.66', '-9984.98554455']
-    get_validation(automaton, words)
+    get_validation(automata, words)
 
 
 if __name__ == '__main__':
